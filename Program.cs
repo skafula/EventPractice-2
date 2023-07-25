ccb74bd18562487e6355e68cd8baa4db909609a0
skafula <79100340+skafula@users.noreply.github.com>
2023-07-25 16:03:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using DelegatePractice;
+
+internal class Program
+{
+    public static void Main()
+    {
+        User.UserAddedEvent += CheckIfAdult;
+        User.UserAddedEvent += user => Console.WriteLine(user.Gender == Genders.Male ? " and male" : " and female"); 
+
+        User male = new(1, "John", 21, Genders.Male);
+        User female = new(2, "Victoria", 17, Genders.Female);
+    }
+    static void CheckIfAdult(User user)
+    {
+        if (user != null)
+            Console.Write(user.Age >= 18 ? $"{user.Name} is adult" : $"{user.Name} is child");
+    }
+}
---
+++ User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatePractice
+{
+    public class User
+    {
+        public static event Action<User> UserAddedEvent;
+
+        private List<User> Users = new List<User>();
+
+        public int Id;
+        public string Name;
+        public int Age;
+        public Genders Gender;
+
+        public User(int id, string name, int age, Genders gender)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Gender = gender;
+            Users.Add(this);
+            UserAddedEvent?.Invoke(this);
+        }
+    }
+     public enum Genders
+    {
+        Male,
+        Female
+    }
+}
